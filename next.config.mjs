e50a30ff52a83019baabdb7ade009f8d6aaba2e8
Sonny Sangha <sonny.sangha@gmail.com>
2024-06-14 14:31:23 +0400

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+
+import autoCert from "anchor-pki/auto-cert/integrations/next";
+
+const withAutoCert = autoCert({
+  enabledEnv: "development",
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withAutoCert(nextConfig);
